eadf42fb28537eeb476108bde46912ecff59aab4
Jp1200 <jprkr96@gmail.com>
2020-04-03 15:17:10 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,49 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
-  }
+    this.state = {
 
+      message: '',
+      messages: []
+    };
+  }
+  handleSubmit = event =>{
+    event.preventDefault();
+    let formData = this.state.message
+    let array = this.state.messages.concat(formData)
+    this.setState({
+      messages: array
+    })
+  }
+  handleInput = event =>{
+
+
+
+
+    this.setState({
+      message: event.target.value
+    }
+  )
+
+    }
+  // handleChars = event =>{
+  //
+  // }
   render() {
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <strong>Your message: </strong>
+        <small>Characters remaining: {this.props.maxChars - this.state.message.length}</small>
+        <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.handleInput}
+
+          type="text" name="message" id="message" />
+        <input type='submit'/>
+        </form>
       </div>
     );
   }
